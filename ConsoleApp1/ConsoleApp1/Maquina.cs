e43efe5fe605eece44026b16037e6767a704f00f
antoniogalleg025 <antoniogalleg2895@gmail.com>
2024-04-03 17:55:57 +0200

    Nuevo metodo NuevoProducto en la clase padre Producto y implementacion de herencia en sus respectivas clases hijas

--- ConsoleApp1/ConsoleApp1/Maquina.cs
+++ ConsoleApp1/ConsoleApp1/Maquina.cs
@@ -8,5 +8,38 @@ namespace ConsoleApp1
 {
     internal class Maquina
     {
+
+
+        public Maquina() { }
+
+        public void AniadirProducto() {
+
+            Console.WriteLine("1-Nuevo producto precioso");
+            Console.WriteLine("2-Nuevo producto alimenticio");
+            Console.WriteLine("3-Nuevo producto electronico");
+            Console.WriteLine("Introduce el producto que quieres agregar: ");
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    ProductoPrecioso productoPrecioso = new ProductoPrecioso();
+
+                    break;
+
+                case 2:
+                    ProductoAlimenticio productoAlimenticio = new ProductoAlimenticio();
+
+                    break;
+
+                case 3:
+                    ProductoElectronico productoElectronico = new ProductoElectronico();
+
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion incorrecta, pulse una tecla para continuar");
+                    break;
+            }
+        }
     }
 }
--- ConsoleApp1/ConsoleApp1/Producto.cs
+++ ConsoleApp1/ConsoleApp1/Producto.cs
@@ -8,5 +8,36 @@ namespace ConsoleApp1
 {
     internal class Producto
     {
+
+        public Producto() { }
+
+        public string Nombre { get; set; }
+
+        public int Unidades { get; set; }
+
+        public double Precio { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Producto(string nombre, int unidades, double precio, string descripcion)
+        {
+            Nombre = nombre;
+            Unidades = unidades;
+            Precio = precio;
+            Descripcion = descripcion;
+        }
+
+        public virtual void NuevoProducto() {
+
+            Console.WriteLine("---------------------Vamos a agregar un producto--------------------");
+            Console.WriteLine("\nIntroduce el nombre de su producto: ");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("\nIntroduce el numero de unidades del producto: ");
+            Unidades = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nCual es el precio por unidad del producto?: ");
+            Precio = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nIntroduce una breve descripcion del producto: ");
+            Descripcion = Console.ReadLine();
+        }
     }
 }
