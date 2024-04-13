5a52d3eb5ed0364c1d3d19687793051c8e20e307
zamozamo7 <zamvaladr@gmail.com>
2024-04-13 19:16:49 +0200

    Comentarios de la clase ProductoAlimenticio

--- ConsoleApp1/ConsoleApp1/ProductoAlimenticio.cs
+++ ConsoleApp1/ConsoleApp1/ProductoAlimenticio.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class ProductoAlimenticio : Producto
+    internal class ProductoAlimenticio : Producto /*Herencia con la clase productos*/
     {
-        public string InfoNutricional { get; set; }
-        public ProductoAlimenticio() { }
+        public string InfoNutricional { get; set; } /*Atríbutos adicionales de los productos alimenticios*/
+        public ProductoAlimenticio() { }/*Constructor vacío*/
 
+        /*Constructor*/
         public ProductoAlimenticio(int id, string nombre, string tipoproducto, int unidades,
             double precio, string descripcion, string infonutricional)
-            : base(id, nombre, tipoproducto, unidades, precio, descripcion)
+            : base(id, nombre, tipoproducto, unidades, precio, descripcion)/*Más la herencia de la clase Producto*/
         {
             TipoProducto = tipoproducto;
             InfoNutricional = infonutricional;
@@ -24,10 +25,11 @@ namespace ConsoleApp1
             descripción_del_producto = descripcion;
         }
 
-
+        /*Método para establecer los atributos de un nuevo producto alimenticio*/
         public override void NuevoProducto(List<Producto> L, int Comprobacion)
         {
-            base.NuevoProducto(L, Comprobacion);
+            base.NuevoProducto(L, Comprobacion);/*LLamamos a la clase padre*/
+            /*Añadimos los atributos que son solo de productos alimenticios*/
             TipoProducto = "Producto Alimenticio";
             Console.WriteLine("Introduce una descripcion a cerca de la informacion nutricional de su producto");
             InfoNutricional = Console.ReadLine();
@@ -35,19 +37,22 @@ namespace ConsoleApp1
             Console.ReadKey();
         }
 
+        /*Método para mostrar los detalles de los productos alimenticios*/
         public override string MostrarDetalles()
         {
-            return $"{base.MostrarDetalles()}\n\tDescripción nutricional: {InfoNutricional}";
+            return $"{base.MostrarDetalles()}\n\tDescripción nutricional: {InfoNutricional}"; /*Llamamos a la clase padre y añadimos los atributos de productos alimenticios*/
         }
 
+        /*Método para mostrar los elementos de forma reducida*/
         public override string MostrarUnElemento()
         {
-            return base.MostrarUnElemento();
+            return base.MostrarUnElemento(); /*LLamando a la clase padre*/
         }
 
+        /*Método para guardar los elementos en un fichero*/
         public override string GuardarDatosFichero()
         {
-            return base.GuardarDatosFichero() + $"{InfoNutricional}";
+            return base.GuardarDatosFichero() + $"{InfoNutricional}"; /*Llamamos a la clase padre y añadimos los atributos de productos alimenticios*/
         }
     }
 }
